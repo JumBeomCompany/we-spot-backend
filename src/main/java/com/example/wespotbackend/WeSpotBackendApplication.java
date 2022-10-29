c8b356b7f9c8c2137d2b0a2cb51d0e24fe3ae9fa
shinjunseok <sjsage522@gmail.com>
2022-10-29 16:33:07 +0900

    First commit by junseok

---
+++ src/main/java/com/example/wespotbackend/WeSpotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.wespotbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeSpotBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeSpotBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wespotbackend/WeSpotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wespotbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeSpotBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
