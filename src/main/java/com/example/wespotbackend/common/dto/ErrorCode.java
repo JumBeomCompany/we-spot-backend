51db57d90ebb261beec4a5c7f132ff7360cbd398
shinjunseok <sjsage522@gmail.com>
2022-10-30 00:33:18 +0900

    feat(#5): 응답 dto 템플릿화 및 에러코드 관리코드 작성

---
+++ src/main/java/com/example/wespotbackend/common/dto/ErrorCode.java
@@ -0,0 +1,19 @@
+package com.example.wespotbackend.common.dto;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+    TEST_CODE(200, "T001", "테스트 에러 코드."),
+    METHOD_NOT_ALLOWED(405, "E001", "지원하는 않는 요청 메서드 입니다.");
+
+    private final int status;
+    private final String code;
+    private final String message;
+
+    ErrorCode(int status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+}
